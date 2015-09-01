a9599b6ecfa740b9e978bc7dd9284fe91f18090f
Brain Dev <brain-dev@risebox.co>
2015-09-01 17:07:50 +0000

    call risebox-api for the first time

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-var b = require('bonescript'),
-    fs = require('fs'),
-    path = require("path");
+var b     = require('bonescript'),
+    fs    = require('fs'),
+    querystring = require('querystring'),
+    path  = require('path'),
+    https = require('https'),
+    env   = require('node-env-file');
 
 var OneWireFolder = "/sys/devices/w1_bus_master1/";
+
+env(__dirname + '/.env');
 
 //var waterTempProbeId = ;
 
@@ -28,9 +33,42 @@ function getWaterTemp(){
       regexp = /([^t]*)t=([^$]*)/;
       result = regexp.exec(x.data);
       /*console.log('result[2] ' + result[2]);*/
-      printValue(parseInt(result[2])/1000);
+      sendMeasure('WTEMP', parseInt(result[2])/1000);
     });
   });
+}
+
+function sendMeasure(metricKey, value){
+  var postData = querystring.stringify({
+    'value' : value
+  });
+  
+  var options = {
+    hostname: 'rbdev-api.herokuapp.com',
+    path: '/api/devices/' + process.env.RISEBOX_KEY + '/metrics/' + metricKey + '/measures',
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'RISEBOX-SECRET': process.env.RISEBOX_SECRET
+    }
+  };
+  
+  var req = https.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  });
+  
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+  
+  // write data to request body
+  req.write(postData);
+  req.end();
 }
 
 setInterval(getWaterTemp, 1000);
