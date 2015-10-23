302517e436823ec7b2ac12ad3ede30b7af197ca2
Brain Dev <brain-dev@risebox.co>
2015-10-23 12:39:48 +0000

    add Adafruit Python Script to dependencies install

--- lib/install-w1-dts.sh
+++ lib/install-w1-dts.sh
@@ -1,4 +1,19 @@
-dtc -O dtb -o BB-W1-00A0.dtbo -b 0 -@ w1.dts
-cp BB-W1-00A0.dtbo /lib/firmware
-echo BB-W1 > /sys/devices/bone_capemgr.9/slots
+ROOT_DIR=/home/risebox/risebox-brain
+cd $ROOT_DIR
+
+if [[ -n $(ls $ROOT_DIR | grep dependencies) ]]; then
+  echo "Dependencies folder exists" 
+else
+  echo "Creating dependencies folder" 
+  mkdir $ROOT_DIR/dependencies
+fi
+
+#dtc -O dtb -o ./dependencies/BB-W1-00A0.dtbo -b 0 -@ ./lib/w1.dts
+#cp ./dependencies/BB-W1-00A0.dtbo /lib/firmware
+#echo BB-W1 > /sys/devices/bone_capemgr.9/slots
+
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git ./dependencies/Adafruit_Python_DHT
+cd ./dependencies/Adafruit_Python_DHT
+sudo apt-get install build-essential python-dev python-openssl
+sudo python setup.py install
 
