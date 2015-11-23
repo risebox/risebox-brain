893bc034c68bad48c86754a8854ca33b0d1f0a68
nna <nico.nardone@gmail.com>
2015-11-23 14:13:03 +0100

    update script restart the service

--- update-brain.sh
+++ update-brain.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-systemctl stop risebox-brain.service
-
 git pull origin master --tags
 
 git checkout tags/$1
 
-systemctl start risebox-brain.service
+systemctl restart risebox-brain.service
