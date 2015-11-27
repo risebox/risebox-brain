63dd7e36f54b4f9600b15c88e59db774c01a19ee
Adrien Thery <adrien.thery@gmail.com>
2015-11-27 14:10:20 +0100

    fix recovery notification url

--- risebox-brain-recovery.sh
+++ risebox-brain-recovery.sh
@@ -6,6 +6,8 @@ apiServer=rbdev-api.herokuapp.com
 timeToWait=10
 rollbackVersion=$(sudo cat ./ROLLBACK)
 deviceKey=$(sudo cat .env | grep "RISEBOX_DEVICE_KEY=" | xargs echo | cut -f 2 -d '=')
+userEmail=$(sudo cat .env | grep "RISEBOX_USER_EMAIL=" | xargs echo | cut -f 2 -d '=')
+userSecret=$(sudo cat .env | grep "RISEBOX_USER_SECRET=" | xargs echo | cut -f 2 -d '=')
 echo "Your deviceKey is ${deviceKey}"
 
 if [[ $(systemctl is-active risebox-brain.service) == "failed" ]]; then
@@ -14,8 +16,7 @@ if [[ $(systemctl is-active risebox-brain.service) == "failed" ]]; then
 
   if [[ $(systemctl is-active risebox-brain.service) == "failed" ]]; then
     echo "After ${timeToWait} secs, the Brain is still failed => Will notify server and rollback"
-    curl http://$apiServer/rollback?device=$deviceKey&version=$rollbackVersion
-    pwd
+    curl https://$apiServer/api/devices/$(deviceKey)/brain_rollback --data "version=$rollbackVersion" -H "Accept: application/json" -H "RISEBOX-USER-EMAIL: $(userEmail)" -H "RISEBOX-USER-SECRET: $(userSecret)"
     sh ./update-brain.sh v$rollbackVersion
 
    # reboot
