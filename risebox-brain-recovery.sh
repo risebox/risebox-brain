26e1ef7fb1df62645d2edcc25a23e8bf199cf3d2
Brain Dev <brain-dev@risebox.co>
2015-11-27 12:31:06 +0100

    fix brain recovery

--- risebox-brain-recovery.sh
+++ risebox-brain-recovery.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+cd /home/risebox/risebox-brain
 echo "Risebox Brain recovery program: now starting"
 
 apiServer=rbdev-api.herokuapp.com
-timeToWait=60
+timeToWait=10
 rollbackVersion=$(sudo cat ./ROLLBACK)
 deviceKey=$(sudo cat .env | grep "RISEBOX_DEVICE_KEY=" | xargs echo | cut -f 2 -d '=')
 echo "Your deviceKey is ${deviceKey}"
 
-if [[ $(systemctl is-failed risebox-brain.service) == "failed" ]]; then
-  echo "Brain service is failed: waiting ${timeToWait} minute to confirm"
+if [[ $(systemctl is-active risebox-brain.service) == "failed" ]]; then
+  echo "Brain service is failed: waiting ${timeToWait} seconds to confirm"
   sleep $timeToWait
 
-  if [[ $(systemctl is-failed risebox-brain.service) == "failed" ]]; then
+  if [[ $(systemctl is-active risebox-brain.service) == "failed" ]]; then
     echo "After ${timeToWait} secs, the Brain is still failed => Will notify server and rollback"
-    curl http://$apiServer/api/rollback?device=$deviceKey&version=$rollbackVersion
+    curl http://$apiServer/rollback?device=$deviceKey&version=$rollbackVersion
+    pwd
+    sh ./update-brain.sh v$rollbackVersion
 
-    ./update_brain.sh rollbackVersion
-
-    reboot
+   # reboot
   fi
 fi
