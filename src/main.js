4266391a533f21ee582bc0fd25c87368100c7e99
root <root@risebox-goldstein.localdomain>
2016-01-28 20:45:16 +0100

    merge

--- src/main.js
+++ src/main.js
@@ -17,9 +17,7 @@ src_path = function(pathString) {
 
 var TimeSyncer = require('./utils/time-syncer');
 var timeSyncer = new TimeSyncer();
-//timeSyncer.sync();
-setInterval(timeSyncer.sync, 10*60*1000);
-
+setTimeout(timeSyncer.sync, 5*60*1000);
 
 var Box      = require('./box');
 var box = new Box({'width': 102.5, 'depth': 46.2, 'probeHeight': 37.7});
--- src/utils/time-syncer.js
+++ src/utils/time-syncer.js
@@ -1,20 +1,41 @@
 var exec = require('child_process').exec;
+var http = require('http');
 
 var TimeSyncer = function() {
   var api = require('../api/api');
   
   this.sync = function(){
-   now = new Date();
-   api.sendLog('info', 'Before sync now is ' + now);
-   exec('ntpdate -s 0.fr.pool.ntp.org', function(error, stdout, stderr) {
-    if (error == null) {
-      api.sendLog('info', 'System time setup with ntpdate');
-      now = new Date();
-      api.sendLog('info', 'After sync now is ' + now);
-    } else {
-      api.sendLog('error', 'Could not update system time with ntpdate');
-    }
-   });
+    ifWebAvailable(function(){
+      console.log("Web is available will sync");
+      ntpSync();
+      setTimeout(this.sync, 10*60*1000);
+    },function(){
+      console.log("No Web so No time sync possible will try again in 10 secs");
+      setTimeout(this.sync, 10*1000);
+    });
+  }
+
+  var ntpSync = function(){
+    now = new Date();
+    api.sendLog('info', 'Before sync now is ' + now);
+    exec('ntpdate -s 0.fr.pool.ntp.org', function(error, stdout, stderr) {
+      if (error == null) {
+        api.sendLog('info', 'System time setup with ntpdate');
+        now = new Date();
+        api.sendLog('info', 'After sync now is ' + now);
+      } else {
+        api.sendLog('error', 'Could not update system time with ntpdate');
+      }
+    });
+  }
+
+  var ifWebAvailable = function(successCb, errorCb){
+    http.get('http://risebox-api.herokuapp.com', function(res) {
+      successCb();
+    }).on('error', function(e) {
+      console.log("Web is not available: got error with message " + e.message);
+      errorCb();
+    });
   }
 }
 
